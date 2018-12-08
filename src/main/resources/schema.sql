6a3ab34f2b9f0de6417bd363f9542016bc67977f
Sakwer <patryk-szczesny@o2.pl>
2018-12-08 22:56:06 +0100

    Radek's changes to schema.sql"

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,13 @@
-DROP TABLE bike_user IF EXISTS;
+DROP TABLE IF EXISTS bike_user;
 CREATE TABLE bike_user (
 id INTEGER NOT NULL AUTO_INCREMENT,
 login VARCHAR(128) NOT NULL UNIQUE,
-password VARCHAR (128) NOT NULL ,
+password VARCHAR (128) NOT NULL,
 e_mail  varchar (128) not null UNIQUE ,
 role VARCHAR (128) NOT NULL ,
 PRIMARY KEY (id)
 );
-drop table bikes if exists;
-create  table bikes(id INTEGER NOT NULL AUTO_INCREMENT, status varchar(255),primary key (id));
+drop table if exists bikes;
+create  table bikes(id INTEGER NOT NULL AUTO_INCREMENT,
+status varchar(255),
+primary key (id));
