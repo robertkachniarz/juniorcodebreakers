03b251b2c8a4524d0c66ccff735f08d5b6b8d3c2
Tadeusz Zalewski <tad.zalewski@gmail.com>
2018-12-09 11:55:14 +0100

    test fix

--- src/test/java/org/juniorcodebreakers/controller/UserControllerTest.java
+++ src/test/java/org/juniorcodebreakers/controller/UserControllerTest.java
@@ -4,9 +4,8 @@ import org.juniorcodebreakers.Main;
 import org.juniorcodebreakers.login.BikeUserRepository;
 import org.juniorcodebreakers.login.Role;
 import org.juniorcodebreakers.model.user.BikeUser;
-import org.juniorcodebreakers.service.BikeUserApiClientImpl;
-import org.juniorcodebreakers.service.bike.BikeRepository;
-import org.junit.jupiter.api.Test;
+import org.juniorcodebreakers.service.BikeUserApiClient;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,101 +22,104 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
-@ContextConfiguration(classes={Main.class})
-class UserControllerTest {
+@ContextConfiguration(classes = {Main.class})
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private BikeUserApiClientImpl bikeUserApiClient;
-    @MockBean
     private BikeUserRepository bikeUserRepository;
+    @MockBean
+    private BikeUserApiClient bikeUserApiClient;
 
     @Test
-    void shouldShowAddUser() throws Exception {
+    public void shouldShowAddUser() throws Exception {
         // given
         // when
         mockMvc.perform(get("/users/add"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/add"));
     }
 
     @Test
-    void shouldAdd() throws Exception {
+    public void shouldAdd() throws Exception {
         // given
         BikeUser bikeUser = new BikeUser();
         bikeUser.setRole(Role.USER.toString());
-       given(bikeUserRepository.save(bikeUser)).willReturn(bikeUser);
+        given(bikeUserRepository.save(bikeUser)).willReturn(bikeUser);
         // when
         mockMvc.perform(post("/users/add"))
-        //then
-               .andExpect(status().isCreated());
+                //then
+                .andExpect(status().isCreated());
         then(bikeUserRepository).should().save(bikeUser);
     }
 
     @Test
-    void shouldShowUserMenuPage() throws Exception {
+    public void shouldShowUserMenuPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/usermenu"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/usermenu"));
     }
 
     @Test
-    void shouldShowAboutUsPage() throws Exception {
+    public void shouldShowAboutUsPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/aboutus"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/aboutus"));
     }
 
     @Test
-    void shouldShowAccountBalancePage() throws Exception {
+    public void shouldShowAccountBalancePage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/accountbalance"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/accountbalance"));
     }
 
     @Test
-    void shouldShowContactPage() throws Exception {
+    public void shouldShowContactPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/contact"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/contact"));
     }
 
     @Test
-    void shouldShowHistoryPage() throws Exception {
+    public void shouldShowHistoryPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/history"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/history"));
     }
 
     @Test
-    void shouldShowMyAccoutnPage() throws Exception {
+    public void shouldShowMyAccoutnPage() throws Exception {
         // given
         BikeUser bikeUser = new BikeUser();
         bikeUser.setLogin("login");
@@ -127,41 +129,41 @@ class UserControllerTest {
         mockMvc.perform(get("/users/login"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(model().attribute("bikeuser",bikeUser))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(model().attribute("bikeuser", bikeUser))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/myaccount"));
     }
 
     @Test
-    void shouldShowRentalPage() throws Exception {
+    public void shouldShowRentalPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/rental"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/rental"));
     }
 
     @Test
-    void shouldShowTopUpAccountPage() throws Exception {
+    public void shouldShowTopUpAccountPage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/topupaccount"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/menuhtml/topupaccount"));
     }
 
     @Test
-    void shouldShowHomePage() throws Exception {
+    public void shouldShowHomePage() throws Exception {
         // given
         // when
         mockMvc.perform(get("/home"))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE+";charset=UTF-8"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
                 .andExpect(view().name("users/home"));
     }
 
