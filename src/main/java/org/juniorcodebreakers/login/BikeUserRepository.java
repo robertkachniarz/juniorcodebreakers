5ea8fb260220c512b1b5014edc1403b76dbe4c61
RadoslawJankowski <radekj1@interia.pl>
2018-12-15 15:21:11 +0100

    change h2 database to mysql

--- src/main/java/org/juniorcodebreakers/login/BikeUserRepository.java
+++ src/main/java/org/juniorcodebreakers/login/BikeUserRepository.java
@@ -1,13 +1,13 @@
 package org.juniorcodebreakers.login;
 
 import org.juniorcodebreakers.model.user.BikeUser;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface BikeUserRepository extends CrudRepository<BikeUser, Long> {
+public interface BikeUserRepository extends JpaRepository<BikeUser, Long> {
 
     Optional<BikeUser> findByLogin(String login);
 }
--- src/main/java/org/juniorcodebreakers/login/RoleRepository.java
+++ src/main/java/org/juniorcodebreakers/login/RoleRepository.java
@@ -1,13 +1,13 @@
 package org.juniorcodebreakers.login;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
     @Query("select r from Role r inner join r.users u where u.login = ?1")
     Set<Role> findRoleByLogin(String login);
